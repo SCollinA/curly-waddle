080926c598f964dbb494c5d19aea15296992385b
Collin Argo <collinargo@gmail.com>
2019-05-18 11:30:05 -0400

    write first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as enzyme from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,8 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders counter component', () => {
+  const app = enzyme.shallow(<App/>);
+  expect(app.render().text()).toBe('Hello World!')
+})
