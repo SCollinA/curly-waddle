7d3540dea62262c0d856483dea19f7eada124a73
Collin Argo <collinargo@gmail.com>
2019-05-18 14:52:55 -0400

    add plus and minus buttons with on click to update count

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -36,6 +36,42 @@ it('renders count from initial state in second h1 as 0', () => {
 
 it('renders counterButtons div as third child of app', () => {
   const app = enzyme.shallow(<App/>);
-  expect(app.childAt(2).is('div')).toBe(true)
-  expect(app.childAt(2).hasClass('counterButtons')).toBe(true)
+  expect(app.childAt(2).is('div')).toBe(true);
+  expect(app.childAt(2).hasClass('counterButtons')).toBe(true);
+})
+
+it('renders + and - in separate h1\'s in counterButtons', () => {
+  const counterButtons = enzyme.shallow(<App/>).childAt(2);
+  expect(counterButtons.childAt(0).is('h1')).toBe(true);
+  expect(counterButtons.childAt(0).render().text()).toBe('+');
+  expect(counterButtons.childAt(1).is('h1')).toBe(true);
+  expect(counterButtons.childAt(1).render().text()).toBe('-');
+})
+
+it('renders counterButtons inline', () => {
+  const counterButtons = enzyme.shallow(<App/>).childAt(2);
+  expect(counterButtons.childAt(0).render().css('display')).toBe('inline');
+  expect(counterButtons.childAt(1).render().css('display')).toBe('inline');
+})
+
+it('increments app state field \'count\' when + is clicked', () => {
+  const app = enzyme.shallow(<App/>);
+  const counterButtons = app.childAt(2)
+  const plusButton = counterButtons.childAt(0);
+  plusButton.simulate('click');
+  expect(app.state('count')).toBe(1);
+})
+
+it('decrements app state field \'count\' when + is clicked', () => {
+  const app = enzyme.shallow(<App/>);
+  const counterButtons = app.childAt(2)
+  const minusButton = counterButtons.childAt(1);
+  minusButton.simulate('click');
+  expect(app.state('count')).toBe(-1);
+})
+
+it('renders counterButtons with pointer cursor', () => {
+  const counterButtons = enzyme.shallow(<App/>).childAt(2);
+  expect(counterButtons.childAt(0).render().css('cursor')).toBe('pointer');
+  expect(counterButtons.childAt(1).render().css('cursor')).toBe('pointer');
 })
--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,33 @@ class App extends React.Component<Props, State> {
       count: 0,
     }
   }
-  // const [count, setCount] = React.useState(0)
+
+  incrementCount = () => this.updateCount(this.state.count + 1)
+  decrementCount = () => this.updateCount(this.state.count - 1)
+
+  updateCount = (count: number) => this.setState({
+    count
+  })
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
         <h1>{this.state.count}</h1>
-        <div className='counterButtons'></div>
+        <div className='counterButtons'>
+          <h1 
+            style={{ display: 'inline', cursor: 'pointer' }}
+            onClick={this.incrementCount}
+          >
+            +
+          </h1>
+          <h1 
+            style={{ display: 'inline', cursor: 'pointer' }}
+            onClick={this.decrementCount}
+          >
+            -
+          </h1>
+        </div>
       </div>
     );
   }
